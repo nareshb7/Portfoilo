53dadb65abcf981ed39170441ce4804c0838a964
nareshb7 <nareshbjava7@gmail.com>
2023-01-14 23:12:28 +0530

    Nav Updated

---
+++ front-end/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Nav from './components/Nav';
+import RoutesComp from './RoutesComp';
+
+function App() {
+  return (
+    <div>
+      <Nav />
+      <RoutesComp />
+    </div>
+  );
+}
+
+export default App;
---
+++ front-end/src/RoutesComp.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const RoutesComp =()=> {
+    return (
+        <div>RoutesComp</div>
+    )
+}
+export default RoutesComp
